6e83e45a727b3e2f5b2fcacd0b9afa80ca557e13
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-03-10 22:38:37 +0900

    Main.js生成

---
+++ backend/PMP/resources/js/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {
+  BrowserRouter as Router ,
+  Switch,
+  Route
+} from "react-router-dom";
+
+function Main(){
+  return(
+    <Router>
+      
+    </Router>
+  )
+}
